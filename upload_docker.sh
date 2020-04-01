5ceab57a52a85934f7eee75f59b2e0a0259bcce9
ricoms <ricardosavii@gmail.com>
2020-04-01 19:48:00 -0300

    [task4] upload the Docker Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ricoms858/ml_cloud_devops
+tag=$(git rev-parse --verify HEAD | sed -r 's/(.{7}).*/\1/g')
 
-# Step 2:  
-# Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag api ${dockerpath}:${tag}
 
-# Step 3:
-# Push image to a docker repository
+docker push ${dockerpath}:${tag}
