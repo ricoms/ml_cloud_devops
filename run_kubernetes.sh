644015855009f0dba03e0fca23f7eacb7153ed9a
ricoms <ricardosavii@gmail.com>
2020-04-01 20:14:42 -0300

    [task6] deploy with kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,13 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ricoms858/ml_cloud_devops
 
-# Step 2
-# Run the Docker Hub container with kubernetes
+kubectl run houseprediction\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=houseprediction
 
+kubectl get pods
 
-# Step 3:
-# List kubernetes pods
-
-# Step 4:
-# Forward the container port to a host
-
+kubectl port-forward houseprediction 8000:80
